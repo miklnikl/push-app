82c808e071392e00505d39412394f87fe6c83eed
Mikhail Zhdanov <mikhailzhdanov@Mikhails-Air.fritz.box>
2025-07-25 16:18:46 +0200

    Add default workout

---
+++ src/modules/workout/constants.ts
@@ -0,0 +1,19 @@
+import type { PushupSet } from "./types";
+
+export const defaultPushupSet: PushupSet[] = [
+    {
+        pushupCount: 10,
+    },
+    {
+        pushupCount: 10,
+    },
+    {
+        pushupCount: 10,
+    },
+    {
+        pushupCount: 10,
+    },
+    {
+        pushupCount: 10,
+    },
+];
